cb1a8323f97045c5687f752706a744d2f847b448
gwirn <71886945+gwirn@users.noreply.github.com>
2024-12-26 17:12:45 +0100

    day 12 part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,10 +1,49 @@
 use std::{collections::VecDeque, fs};
 const DIRECTIONS: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+const DIAGONALS: [(i32, i32); 4] = [(1, 1), (1, -1), (-1, -1), (-1, 1)];
 
 /*
-flood fill after finding new starting point
+calculate the number of faces by calculating the number of edges
 */
-fn flood(grid: &mut Vec<Vec<&str>>, yd: usize, xd: usize) -> usize {
+fn edge_test(plant_fields: &Vec<(usize, usize)>) -> usize {
+    // only type conversion
+    let pf_i32: Vec<(i32, i32)> = plant_fields
+        .iter()
+        .map(|x| (x.0 as i32, x.1 as i32))
+        .collect();
+    let mut egdes = 0;
+    // for each plant field check if if L fields are the same and the diagonal is not or if the
+    // L fields are different - both shows an edge
+    for p in plant_fields {
+        for i in 0..4 {
+            // first L field
+            let d0 = (p.0 as i32 + DIRECTIONS[i].0, p.1 as i32 + DIRECTIONS[i].1);
+            // diagonal field
+            let mut d1 = (0, 0);
+            if i == 3 {
+                d1.0 = p.0 as i32 + DIRECTIONS[0].0;
+                d1.1 = p.1 as i32 + DIRECTIONS[0].1;
+            } else {
+                d1.0 = p.0 as i32 + DIRECTIONS[i + 1].0;
+                d1.1 = p.1 as i32 + DIRECTIONS[i + 1].1;
+            }
+            // second L field
+            let di0 = (p.0 as i32 + DIAGONALS[i].0, p.1 as i32 + DIAGONALS[i].1);
+            // if the needed fields exist
+            let d0_test = pf_i32.contains(&d0);
+            let d1_test = pf_i32.contains(&d1);
+            let di0_test = pf_i32.contains(&di0);
+            if d0_test && d1_test && !di0_test || !d0_test && !d1_test {
+                egdes += 1;
+            }
+        }
+    }
+    egdes
+}
+/*
+flood fill after finding new starting point and calculate cost
+*/
+fn flood(grid: &mut Vec<Vec<&str>>, yd: usize, xd: usize) -> (usize, usize) {
     let mut area = 0;
     let mut perimeter = 0;
     // find next new field to start (first not visited field)
@@ -25,16 +64,13 @@ fn flood(grid: &mut Vec<Vec<&str>>, yd: usize, xd: usize) -> usize {
     // the visited fields of the current plant
     let mut visited: Vec<(usize, usize)> = Vec::new();
     loop {
-        if queue.is_empty() {
-            break;
-        }
         let field = match queue.pop_front() {
             Some(v) => {
                 visited.push(v);
                 area += 1;
                 v
             }
-            None => panic!("pop from empty queue"),
+            None => break,
         };
         let mut peri = 4;
         // check for all directions if there is the same plant
@@ -61,8 +97,9 @@ fn flood(grid: &mut Vec<Vec<&str>>, yd: usize, xd: usize) -> usize {
     for i in &visited {
         grid[i.0][i.1] = ".";
     }
+    let edges = edge_test(&visited);
     // calculate cost
-    area * perimeter
+    (area * perimeter, area * edges)
 }
 pub fn day12() {
     let raw_input = fs::read_to_string("./inputs/day12.txt").expect("cant open file");
@@ -76,7 +113,8 @@ pub fn day12() {
     let ydim = grid.len();
     let xdim = grid[0].len();
     // total fence cost
-    let mut cost = 0;
+    let mut cost1 = 0;
+    let mut cost2 = 0;
     loop {
         // if all grid spaces are mask it's finished
         if &grid
@@ -87,7 +125,10 @@ pub fn day12() {
         {
             break;
         }
-        cost += flood(&mut grid, ydim, xdim);
+        let (p_cost, e_cost) = flood(&mut grid, ydim, xdim);
+        cost1 += p_cost;
+        cost2 += e_cost;
     }
-    println!("Total cost: {}", cost);
+    println!("Total cost part 1: {}", cost1);
+    println!("Total cost part 2: {}", cost2);
 }
