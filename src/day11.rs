27ab67157c2a0d2a7767cbea87c27759cf3c77a1
gwirn <71886945+gwirn@users.noreply.github.com>
2024-12-28 10:39:10 +0100

    day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,9 +1,47 @@
-use std::fs;
+use std::{collections::HashMap, fs};
+
+/*
+more efficient way than for part 1
+*/
+fn mod_input_p2(input_parsed: Vec<i64>, blinks: usize) -> usize {
+    // stones and their appearance count
+    let mut stones: HashMap<i64, usize> = HashMap::new();
+    // insert initial stones
+    for i in &input_parsed {
+        stones.insert(*i, 1);
+    }
+    for _ in 0..blinks {
+        // stones and their appearance count
+        let mut n_stones: HashMap<i64, usize> = HashMap::new();
+        // change all (unique) existing stones according to the rules and update their count
+        for s in stones.keys() {
+            let s_string = format!("{}", s);
+            if *s == 0 {
+                *n_stones.entry(1 as i64).or_insert(0) += stones
+                    .get(&(0 as i64))
+                    .expect(&format!("Never seen stone [{}]", s));
+            } else if s_string.len() % 2 == 0 {
+                let half_idx = (s_string.len() / 2) as usize;
+                let s_split = s_string.split_at(half_idx);
+                let left = s_split.0.parse::<i64>().expect("cant parse to i64");
+                let right = s_split.1.parse::<i64>().expect("cant parse to i64");
+                *n_stones.entry(left).or_insert(0) +=
+                    stones.get(s).expect(&format!("Never seen stone [{}]", s));
+                *n_stones.entry(right).or_insert(0) +=
+                    stones.get(s).expect(&format!("Never seen stone [{}]", s));
+            } else {
+                *n_stones.entry(s * 2024).or_insert(0) +=
+                    stones.get(s).expect(&format!("Never seen stone [{}]", s));
+            }
+        }
+        // update the stones appearance map
+        stones = n_stones;
+    }
+    stones.values().sum::<usize>()
+}
 
 // recursively modify the input
-fn mod_input_p1(input_parsed: Vec<i64>, mut count: usize, max_iter: usize) -> Vec<i64> {
-    // the round started
-    println!("Count: {:?}", count);
+fn mod_input_p1(input_parsed: &Vec<i64>, mut count: usize, max_iter: usize) -> usize {
     // new vec
     let mut moded: Vec<i64> = vec![];
     for i in input_parsed.iter() {
@@ -23,9 +61,9 @@ fn mod_input_p1(input_parsed: Vec<i64>, mut count: usize, max_iter: usize) -> Ve
     count += 1;
     // stop if number of iterations is reached
     if count > max_iter {
-        return moded;
+        return moded.len();
     }
-    mod_input_p1(moded, count, max_iter)
+    mod_input_p1(&moded, count, max_iter)
 }
 
 pub fn day11() {
@@ -36,6 +74,8 @@ pub fn day11() {
         .filter(|y| y.len() > 0)
         .map(|x| x.parse::<i64>().expect("cant parse to i64"))
         .collect::<Vec<i64>>();
-    let moded = mod_input_p1(input_parsed, 0, 24);
-    println!("{:?}", moded.len());
+    let moded = mod_input_p1(&input_parsed, 0, 24);
+    let moded2 = mod_input_p2(input_parsed, 75);
+    println!("Part 1: {:?}", moded);
+    println!("Part 2: {:?}", moded2);
 }
